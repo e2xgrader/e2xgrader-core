39fa94f29bdd6779c41fd2d0b380354d5357480b
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-06-02 17:02:41 +0200

    Update test

--- e2xgrader_core/tests/test_handlers.py
+++ e2xgrader_core/tests/test_handlers.py
@@ -3,9 +3,11 @@ import json
 
 async def test_get_username(jp_fetch):
     # When
-    response = await jp_fetch("e2xgrader", "api", "username")
+    response = await jp_fetch("e2xgrader", "api", "course-info")
 
     # Then
     assert response.code == 200
     payload = json.loads(response.body)
     assert "username" in payload
+    assert "course_id" in payload
+    assert "root" in payload
