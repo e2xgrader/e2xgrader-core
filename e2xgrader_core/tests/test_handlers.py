f86564162f8c2ea6b688ef8ff546b252720af098
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-06-02 16:53:51 +0200

    Update server extension test

--- e2xgrader_core/tests/test_handlers.py
+++ e2xgrader_core/tests/test_handlers.py
@@ -1,13 +1,11 @@
 import json
 
 
-async def test_get_example(jp_fetch):
+async def test_get_username(jp_fetch):
     # When
-    response = await jp_fetch("e2xgrader-core", "get-example")
+    response = await jp_fetch("e2xgrader", "api", "username")
 
     # Then
     assert response.code == 200
     payload = json.loads(response.body)
-    assert payload == {
-        "data": "This is /e2xgrader-core/get-example endpoint!"
-    }
+    assert "username" in payload
