6dcf9d3064107e417384bc25801ac0f886ae5954
LUC4R4T0R <luc4r4t0r@gmail.com>
2026-09-17 14:05:07 +0000

    added overload to pass toolbar-item settings to toolbar widgets

--- src/notebook-toolbar/SecondaryToolbarWidgetRegistry.ts
+++ src/notebook-toolbar/SecondaryToolbarWidgetRegistry.ts
@@ -9,6 +9,18 @@ import {
  * this extended ToolbarWidgetRegistry allows special toolbar-widgets to be registered, that receive individual configuration from settings as 'toolbarItem'
  */
 export class SecondaryToolbarWidgetRegistry extends ToolbarWidgetRegistry {
+  /**
+   * overload of the original createWidget() to enable passing settings to the widget
+   * @param widgetFactory name of the widget-factory (toolbar-item name/id)
+   * @param widget parent widget
+   * @param toolbarItem settings of the toolbar-item
+   */
+  createWidget(widgetFactory: string, widget: Widget, toolbarItem: ToolbarRegistry.IWidget): Widget {
+    const factory = this._widgets.get(widgetFactory)?.get(toolbarItem.name);
+    if(factory) return (factory as SecondaryToolbarWidgetRegistry.ToolbarWidgetFactory<Widget>)(widget, toolbarItem); //if a factory was registered -> call it and pass the parent widget as well es the widget's settings
+    return super.createWidget(widgetFactory, widget, toolbarItem);
+  }
+
   addFactory<T extends Widget>(
     widgetFactory: string,
     toolbarItemName: string,
